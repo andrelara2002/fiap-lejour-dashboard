511e0607749ccdfd913c14fa7a2d8b9b7f08044c
jose barreto teixeira de paula <jose.barreto@fieldcorp.com.br>
2020-11-18 16:11:34 -0300

    Update index.js

--- src/Components/DetalhesComponent/index.js
+++ src/Components/DetalhesComponent/index.js
@@ -9,6 +9,12 @@ import wedding_icon from "../../Images/wedding-icon.svg";
 import user_icon from "../../Images/user-icon.svg";
 import agendamentos_icon from "../../Images/agendamentos-icon.svg";
 
+import appointmentDatabase from "../../data/appointment.database.json";
+import invoiceDatabase from "../../data/invoice.database.json";
+import userDatabase from "../../data/user.database.json";
+import wenddingDatabase from "../../data/wendding.database.json";
+import wenddingFavoritesDatabase from "../../data/wendding-favorites.database.json";
+
 class DetalhesComponent extends Component {
   constructor(props) {
     super();
@@ -48,92 +54,116 @@ class DetalhesComponent extends Component {
   }
 
   loadData() {
-    api.get("user?limit=10").then(response => {
-      const user_data = response.data;
-      var listId = [];
-      var dataList = [];
-      user_data.map((valor, idx) => {
-        listId = listId.concat(user_data[idx].ID);
-        dataList = dataList.concat(user_data[idx].CREATED_AT);
+    api.get("user?limit=10")
+      .then(response => {
+        return response;
+      })
+      .catch((response) => {
+        response.data = userDatabase;
+        return response;
+      })
+      .then((response) => {
+        const user_data = response.data;
+        var listId = [];
+        var dataList = [];
+        user_data.map((valor, idx) => {
+          listId = listId.concat(user_data[idx].ID);
+          dataList = dataList.concat(user_data[idx].CREATED_AT);
+
+          this.setState({
+            usuario: {
+              idUsuario: listId,
+              data: dataList
+            }
+          });
+        });
+      });
+
+    api.get("wedding?limit=10")
+      .then((response) => {
+        return response;
+      })
+      .catch((response) => {
+        response.data = wenddingDatabase;
+        return response;
+      })
+      .then(response => {
+        const wedding_data = response.data;
+
+        var id_casamento_list = [];
+        var id_owner_list = [];
+        var nr_convidados_list = [];
+        var estilo_list = [];
+        var date_list = [];
+        var valor_list = [];
+
+        wedding_data.map((valor, idx) => {
+          id_casamento_list = id_casamento_list.concat(wedding_data[idx].ID);
+          id_owner_list = id_owner_list.concat(wedding_data[idx].OWNER_ID);
+          nr_convidados_list = nr_convidados_list.concat(
+            wedding_data[idx].NUMBER_OF_GUESTS
+          );
+          estilo_list = estilo_list.concat(wedding_data[idx].STYLE);
+          date_list = date_list.concat(wedding_data[idx].WEDDING_DATE);
+          valor_list = valor_list.concat(wedding_data[idx].BUDGET);
+        });
 
         this.setState({
-          usuario: {
-            idUsuario: listId,
-            data: dataList
+          casamentos: {
+            idUsuario: id_owner_list,
+            idCasamento: id_casamento_list,
+            nrConvidados: nr_convidados_list,
+            estilo: estilo_list,
+            nrOrcamento: valor_list,
+            data: date_list
           }
         });
       });
-    });
 
-    api.get("wedding?limit=10").then(response => {
-      const wedding_data = response.data;
+    api.get("appointment?limit=10")
+      .then((response) => {
+        return response;
+      })
+      .catch((response) => {
+        response.data = appointmentDatabase;
+        return response;
+      })
+      .then(response => {
+        const appointment_data = response.data;
+        var id_list = [];
+        var id_casamento_list = [];
+        var id_distribuidor_list = [];
+        var status_list = [];
+        var categoria_fornecedor_list = [];
 
-      var id_casamento_list = [];
-      var id_owner_list = [];
-      var nr_convidados_list = [];
-      var estilo_list = [];
-      var date_list = [];
-      var valor_list = [];
+        appointment_data.map((valor, idx) => {
+          id_list = id_list.concat(appointment_data[idx].ID);
 
-      wedding_data.map((valor, idx) => {
-        id_casamento_list = id_casamento_list.concat(wedding_data[idx].ID);
-        id_owner_list = id_owner_list.concat(wedding_data[idx].OWNER_ID);
-        nr_convidados_list = nr_convidados_list.concat(
-          wedding_data[idx].NUMBER_OF_GUESTS
-        );
-        estilo_list = estilo_list.concat(wedding_data[idx].STYLE);
-        date_list = date_list.concat(wedding_data[idx].WEDDING_DATE);
-        valor_list = valor_list.concat(wedding_data[idx].BUDGET);
+          id_casamento_list = id_casamento_list.concat(
+            appointment_data[idx].WEDDING_ID
+          );
+
+          id_distribuidor_list = id_distribuidor_list.concat(
+            appointment_data[idx].VENDOR_ID
+          );
+
+          status_list = status_list.concat(appointment_data[idx].STATUS);
+
+          categoria_fornecedor_list = categoria_fornecedor_list.concat(
+            appointment_data[idx].VENDOR_CATEGORY
+          );
+        });
+
+        this.setState({
+          appointment: {
+            id: id_list,
+            id_casamento: id_casamento_list,
+            id_distribuidor: id_distribuidor_list,
+            status: status_list,
+            categoria_fornecedor: categoria_fornecedor_list
+          }
+        });
       });
-
-      this.setState({
-        casamentos: {
-          idUsuario: id_owner_list,
-          idCasamento: id_casamento_list,
-          nrConvidados: nr_convidados_list,
-          estilo: estilo_list,
-          nrOrcamento: valor_list,
-          data: date_list
-        }
-      });
-    });
-
-    api.get("appointment?limit=10").then(response => {
-      const appointment_data = response.data;
-      var id_list = [];
-      var id_casamento_list = [];
-      var id_distribuidor_list = [];
-      var status_list = [];
-      var categoria_fornecedor_list = [];
-
-      appointment_data.map((valor, idx) => {
-        id_list = id_list.concat(appointment_data[idx].ID);
-
-        id_casamento_list = id_casamento_list.concat(
-          appointment_data[idx].WEDDING_ID
-        );
-
-        id_distribuidor_list = id_distribuidor_list.concat(
-          appointment_data[idx].VENDOR_ID
-        );
-
-        status_list = status_list.concat(appointment_data[idx].STATUS);
-
-        categoria_fornecedor_list = categoria_fornecedor_list.concat(
-          appointment_data[idx].VENDOR_CATEGORY
-        );
-      });
-
-      this.setState({
-        appointment: {
-          id: id_list,
-          id_casamento: id_casamento_list,
-          id_distribuidor: id_distribuidor_list,
-          status: status_list,
-          categoria_fornecedor: categoria_fornecedor_list
-        }
-      });
-    });
   }
 
   redirect = num => {
